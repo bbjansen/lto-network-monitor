48a239d43b2afbb94eab2c98d0d255433956959e
bbjansen <bobjansen@pm.me>
2020-01-17 18:52:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# LTO Node Monitor
+A nifty cron script written in Node.JS that grabs all known peers of the LTO network and tracks data on availbility, ports, geolocation and status.
+## Requirements
+- Node.Js 8+
+- SQLite 3
+- Cron
+
+## .env variables
+Create ``.env`` file in the root directory:
+
+```
+SEED_NODE=172.104.147.148:6868
+DB_FILE=./db.sqlite3
+CRON_DISCOVER=* * * * *
+CRON_PING=0 0 * * *
+CRON_SCAN=0 0 * * *
+CRON_LOCATE=0 0 * * 0
+```
---
+++ app.js
@@ -0,0 +1,130 @@
+// Copyright (c) 2020, BB. Jansen
+//
+// Please see the included LICENSE file for more information.
+
+'use strict'
+
+// Load .env
+require('dotenv').config()
+
+// Setup logger
+require('console-stamp')(console, {
+  pattern: 'dd/mm/yyyy HH:MM:ss.l',
+  colors: {
+      stamp: 'green',
+      label: 'white',
+  }
+})
+
+// Load Libraries
+const cron = require('node-cron')
+const DB = require('./lib/db')
+const Network = require('./lib/network')
+const db = new DB()
+const network = new Network()
+
+// Start
+init()
+
+// Init
+async function init() {
+  await db.createTables()
+
+  // Check for patient zero
+  let seedNode = process.env.SEED_NODE
+
+  if(!seedNode) {
+
+    console.error('no seed node address specified in .env [ip:port]')
+    return
+
+  } else {
+
+    const checkSeed = await db.selectNode(seedNode)
+
+    if(checkSeed.length <= 0) {
+      await db.insertNode({
+        address: '/' + seedNode,
+        lastSeen: Date.now()
+      })
+      
+    } else {
+      console.info('[seed] ' + seedNode)
+    }
+  }
+
+  // Set cron jobs
+  cron.schedule(process.env.CRON_DISCOVER || '* * * * *', () => {
+    discoverNodes()
+  })
+
+  cron.schedule(process.env.CRON_PING || '0 0 * * *', () => {
+    pingNodes()
+  })
+
+  cron.schedule(process.env.CRON_SCAN || '0 0 * * *', () => {
+    scanNodes()
+  })
+
+  cron.schedule(process.env.CRON_LOCATE || '0 0 * * 0', () => {
+    locateNodes()
+  })
+}
+
+// Discover nodes in the network
+async function discoverNodes() {
+  // Get all stored nodes that have been last seen 24 hours +
+  let knownNodes = await db.selectNodes()
+
+  // collect peer node for each stored node
+  knownNodes.map(async node => {
+    let peerList = await network.getPeers(node.address)
+
+    // if not peer list, no connection
+    if (peerList.length > 0) {
+    
+      // store/update
+      peerList.map(async node => {
+        await db.insertNode(node)
+      })
+    }
+  })
+}
+
+async function pingNodes() {
+  // Get all stored nodes
+  let knownNodes = await db.selectNodes()
+
+  // check status for each stored node
+  knownNodes.map(async node => {
+
+    let nodestatus = await network.getStatus(node.address)
+    await db.updatePing(node.address, nodestatus)
+  })
+}
+
+async function scanNodes() {
+  // Get all stored nodes that have been last seen 24 hours +
+  let knownNodes = await db.selectNodes()
+
+  // check ports for each stored node
+  knownNodes.map(async node => {
+
+    let portStatus = await network.checkPort(node.address)
+    await db.updatePort(node.address, portStatus)
+  })
+}
+
+async function locateNodes() {
+  // Get all stored nodes
+  let knownNodes = await db.selectNodes()
+
+  // check status for each stored node
+  knownNodes.map(async node => {
+
+    let geoData = await network.locateNode(node.address)
+
+    await db.updateGeo(node.address, geoData)
+  })
+}
+
